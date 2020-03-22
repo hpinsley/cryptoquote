adce0b16630b104661adb59c7794a5965ef07f5b
Howard Pinsley <hpinsley@gmail.com>
2020-03-21 21:32:36 -0400

    Switch to play mode

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import '../App.css';
-import {GameStates} from '../models/state';
+import {GameStates} from '../models/gameModels';
 import NoPuzzle from './NoPuzzle';
 import Playing from './Playing';
 
@@ -8,11 +8,18 @@ export default function Game() {
 
   const [gameState, setGameState] = useState(GameStates.NO_PUZZLE)
   const [cypher, setCypher] = useState('');
+  const [keymap, setKeymap] = useState(buildEmptyMap());
+
+  function setCypherAndPlay(encryptedText: string)
+  {
+    setCypher(encryptedText);
+    setGameState(GameStates.PLAYING);
+  }
 
   function renderView() {
     switch(gameState) {
       case GameStates.NO_PUZZLE:
-        return (<NoPuzzle setCypher={setCypher} />);
+        return (<NoPuzzle setCypher={setCypherAndPlay} />);
       case GameStates.PLAYING:
         return (<Playing />)
     }
@@ -24,4 +31,17 @@ export default function Game() {
       <div>The cypher is {cypher}</div>
     </div>
   );
+}
+
+function buildEmptyMap() : Map<string, string>
+{
+  const map = new Map<string, string>();
+
+  for (var i = 0; i<26; ++i)
+  {
+    var key = String.fromCharCode(65 + i);
+    map.set(key, "");
+  }
+
+  return map;
 }
