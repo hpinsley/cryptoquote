69e081b1650820be249f4610b415341dcb0938d1
Howard Pinsley <hpinsley@gmail.com>
2020-04-05 15:15:16 -0400

    Add fanfare for win

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,10 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import '../App.css';
 import reward from '../images/reward-01.jpg';
+import UIfx from 'uifx'
+import fanfareSound from '../sounds/fanfare01.wav'
 
 export type ProgressProps = {
   remaining: number;
 }
+
+const fanfare = new UIfx(
+  fanfareSound,
+  {
+    volume: 0.4, // number between 0.0 ~ 1.0
+    throttleMs: 100
+  }
+)
 
 export default function Progress(props:ProgressProps) 
 {
@@ -14,9 +24,10 @@ export default function Progress(props:ProgressProps)
       //console.log(`In useEffect callback with remaining = ${props.remaining} and awardShow = ${awardShown}`)
       if (props.remaining === 0 && !awardShown) 
       {
+        fanfare.play(1.0);
         setTimeout(() => {
             setAwardShown(true);
-        }, 2500)
+        }, 2500);
       }
     });
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+
+// I included these "modules" so typescript would let me import sound files as per
+// https://stackoverflow.com/questions/56782452/how-to-fix-module-not-found-for-audio-files-using-file-loader-images-css-an
+declare module '*.mp3' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.wav' {
+    const src: string;
+    export default src;
+}
